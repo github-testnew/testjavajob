871e3eb453d7924b2ea7bafe3e469b8f10601b6a
kedar-gopale <109338688+kedar-gopale@users.noreply.github.com>
2023-07-05 16:09:34 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,7 +1,7 @@
  terraform {
        backend "remote" {
          # The name of your Terraform Cloud organization.
-         organization = "github-testnew"
+         organization = "testjavajob"
 
          # The name of the Terraform Cloud workspace to store Terraform state files in.
          workspaces {
